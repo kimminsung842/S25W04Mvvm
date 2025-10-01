064e331b26dc143868eb7b63e316093d57f20d71
domi12s <117494918+domi12s@users.noreply.github.com>
2025-10-01 14:28:33 +0900

    🚀SharedFlow 사용

--- app/src/main/java/kr/ac/kumoh/s20210181/s25w04mvvm/CounterViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20210181/s25w04mvvm/CounterViewModel.kt
@@ -1,8 +1,12 @@
 package kr.ac.kumoh.s20210181.s25w04mvvm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class CounterViewModel : ViewModel() {
     //private val _counterState = mutableStateOf(CounterModel(0))
@@ -10,12 +14,20 @@ class CounterViewModel : ViewModel() {
     //val counterState: State<CounterModel> = _counterState
     val counterState: StateFlow<CounterModel> = _counterState
 
+    private val _events = MutableSharedFlow<String>()
+    val events = _events.asSharedFlow()
+
 //    fun setCounter(newValue: Int) {
 //        _counterState.value = CounterModel(newValue)
 //    }
 
     fun addCounter() {
-        _counterState.value = CounterModel(_counterState.value.count + 1)
+        viewModelScope.launch {
+            _counterState.value = CounterModel(_counterState.value.count + 1)
+            if (_counterState.value.count == 5) {
+                _events.emit("카운터 5에 도달")
+            }
+        }
     }
 
     fun subCounter() {
@@ -23,6 +35,9 @@ class CounterViewModel : ViewModel() {
     }
 
     fun resetCounter() {
-        _counterState.value = CounterModel(0)
+        viewModelScope.launch {
+            _counterState.value = CounterModel(0)
+            _events.emit("카운터 초기화")
+        }
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20210181/s25w04mvvm/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20210181/s25w04mvvm/MainActivity.kt
@@ -7,11 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import kr.ac.kumoh.s20210181.s25w04mvvm.ui.theme.S25W04MvvmTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +41,21 @@ fun MainScreen(
     //val counterState = viewModel.counterState.value
     val counterState by viewModel.counterState.collectAsStateWithLifecycle()
 
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    // key1에 아무 값도 주지 않아서 (Unit 지정), 최초 한 번만 실행
+    LaunchedEffect(key1 = Unit) {
+        viewModel.events.collectLatest { message ->
+            scope.launch {
+                snackbarHostState.showSnackbar(message)
+            }
+        }
+    }
+
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        snackbarHost = {SnackbarHost(snackbarHostState)}
+        ) { innerPadding ->
         Counter(
             modifier = Modifier.padding(innerPadding),
             count = counterState.count,
